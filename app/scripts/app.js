fecb23c79fecb51107d6d2d5b8fd651dbc627103
Tim Kaufeldt <tim.kaufeldt@gmail.com>
2015-04-02 15:15:51 +0000

    Converted landing page to angular + shuffle

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,21 +4,33 @@
  //require('./profile');
 
  angular.module('BlocJams', []).controller('Landing.controller', ['$scope', function($scope) {
+  $scope.bjText = "Bloc Jams";
   $scope.subText = "Turn the music up!";
 
     $scope.subTextClicked = function() {
       $scope.subText += '!';
     };
 
-      $scope.albumURLs = [
-        '/images/album-placeholders/album-1.jpg',
-         '/images/album-placeholders/album-2.jpg',
-         '/images/album-placeholders/album-3.jpg',
-         '/images/album-placeholders/album-4.jpg',
-         '/images/album-placeholders/album-5.jpg',
-         '/images/album-placeholders/album-6.jpg',
-         '/images/album-placeholders/album-7.jpg',
-         '/images/album-placeholders/album-8.jpg',
-         '/images/album-placeholders/album-9.jpg',
-      ];
+    $scope.albumURLs = [
+             '/images/album-placeholders/album-1.jpg',
+             '/images/album-placeholders/album-2.jpg',
+             '/images/album-placeholders/album-3.jpg',
+             '/images/album-placeholders/album-4.jpg',
+             '/images/album-placeholders/album-5.jpg',
+             '/images/album-placeholders/album-6.jpg',
+             '/images/album-placeholders/album-7.jpg',
+             '/images/album-placeholders/album-8.jpg',
+             '/images/album-placeholders/album-9.jpg',
+          ];
+
+    function shuffle(o) {
+      for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+        return o;
+    };
+
+    $scope.bjTextClicked = function() {
+      shuffle($scope.albumURLs);
+      
+    };
+
  }]);
