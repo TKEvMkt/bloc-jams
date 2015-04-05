e582a7ad986230a0cbd092200c3c22a9d407957f
Tim Kaufeldt <tim.kaufeldt@gmail.com>
2015-04-05 00:08:25 +0000

    Converted album view to Angular

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -41,6 +41,13 @@
     controller: 'Collection.controller',
     templateUrl: '/templates/collection.html'
    });
+
+    $stateProvider.state('album', {
+     url: '/album',
+     templateUrl: '/templates/album.html',
+     controller: 'Album.controller'
+   });
+
    
  }]);
  
@@ -84,3 +91,37 @@
      $scope.albums.push(angular.copy(albumPicasso));
    }
 }]);
+
+ blocJams.controller('Album.controller', ['$scope', function($scope) {
+   $scope.album = angular.copy(albumPicasso);
+
+   var hoveredSong = null;
+      var playingSong = null;
+ 
+      $scope.onHoverSong = function(song) {
+        hoveredSong = song;
+      };
+ 
+      $scope.offHoverSong = function(song) {
+         hoveredSong = null;
+      };
+
+      $scope.getSongState = function(song) {
+         if (song === playingSong) {
+           return 'playing';
+         }
+         else if (song === hoveredSong) {
+           return 'hovered';
+         }
+         return 'default';
+      };
+
+      
+        $scope.playSong = function(song) {
+          playingSong = song;
+        };
+     
+        $scope.pauseSong = function(song) {
+          playingSong = null;
+        };
+ }]);
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -310,6 +310,13 @@ var updateSeekPercentage = function($seekBar, event) {
     controller: 'Collection.controller',
     templateUrl: '/templates/collection.html'
    });
+
+    $stateProvider.state('album', {
+     url: '/album',
+     templateUrl: '/templates/album.html',
+     controller: 'Album.controller'
+   });
+
    
  }]);
  
@@ -353,6 +360,40 @@ var updateSeekPercentage = function($seekBar, event) {
      $scope.albums.push(angular.copy(albumPicasso));
    }
 }]);
+
+ blocJams.controller('Album.controller', ['$scope', function($scope) {
+   $scope.album = angular.copy(albumPicasso);
+
+   var hoveredSong = null;
+      var playingSong = null;
+ 
+      $scope.onHoverSong = function(song) {
+        hoveredSong = song;
+      };
+ 
+      $scope.offHoverSong = function(song) {
+         hoveredSong = null;
+      };
+
+      $scope.getSongState = function(song) {
+         if (song === playingSong) {
+           return 'playing';
+         }
+         else if (song === hoveredSong) {
+           return 'hovered';
+         }
+         return 'default';
+      };
+
+      
+        $scope.playSong = function(song) {
+          playingSong = song;
+        };
+     
+        $scope.pauseSong = function(song) {
+          playingSong = null;
+        };
+ }]);
 
 });
 
